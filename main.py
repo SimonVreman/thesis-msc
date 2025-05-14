d0ac9f7b7ba29767375403272da03e26940efcb2
Simon Vreman <info@simonvreman.nl>
2025-05-14 08:40:22 +0200

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from agents import Agent, Runner
+from phoenix.otel import register
+
+tracer_provider = register(
+    project_name="thesis",
+    auto_instrument=True,
+)
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are a helpful assistant",
+    model="litellm/gemini/gemini-2.0-flash-lite",
+)
+
+result = Runner.run_sync(agent, "Write a haiku about recursion in programming.")
+
+print(result.final_output)
